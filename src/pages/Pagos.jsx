2821828ed178cab00ce17fa288fc0f88eb0c8974
Marcos Araya <65575654+jaffar58@users.noreply.github.com>
2022-11-27 19:56:55 -0300

    Update Pagos.jsx

--- src/pages/Pagos.jsx
+++ src/pages/Pagos.jsx
@@ -1,17 +1,53 @@
 import React from 'react'
 import { Button } from '@mui/material'
+import Form from 'react-bootstrap/Form'
 
 import NavBar from '../components/nav_bar'
-import LogoUSM from '../assets/usm.png'
 
 export const Pagos = () => {
   return (
-    <div className='page'>
-      <h1 className='page__title'>Frontend Sample App</h1>
-      <NavBar />
-      <h2>Página Interna</h2>
-      <img src={LogoUSM} height={100} alt='logo usm' /><br />
-      <Button variant='contained'>Material UI Button</Button>
+    <div>
+      <div className='page'>
+        <h1 className='page__title'>Pagar Gastos Comunes</h1>
+        <NavBar />
+      </div>
+      <div className='Pagar'>
+        <Form>
+          <Form.Group className="mb-3" controlId="Banco">
+            <Form.Label>Banco</Form.Label>
+            <Form.Select id="Banco">
+              <option>Banco 1</option>
+              <option>Banco 2</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="NumeroTarjeta">
+            <Form.Label>Numero de tarjeta</Form.Label>
+            <Form.Select id="NumeroTarjeta">
+              <option>Tarjeta 1</option>
+              <option>Tarjeta 2</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="FechaVencimiento">
+            <Form.Label>Fecha Vencimiento</Form.Label>
+            <Form.Select id="FechaVencimiento">
+              <option>Espacio Comun 1</option>
+              <option>Espacio Comun 2</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="CVV">
+            <Form.Label>CVV</Form.Label>
+            <Form.Control ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="NombreTitular">
+            <Form.Label>Nombre Titular</Form.Label>
+            <Form.Control id="NombreTitular"></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="Correo">
+            <Form.Label>Correo</Form.Label>
+            <Form.Control id="Correo"></Form.Control>
+          </Form.Group>
+        </Form>
+        </div>
     </div>
   )
 }
